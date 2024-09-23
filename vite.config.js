9befcd5c587883bb415d84f03268df524f4f2147
shahaf noked <shahafnoked@gmail.com>
2024-09-23 15:02:07 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  // base: '/Interactive-card-form/', change on deploy
+  base: '/ecommerce/',
 })
