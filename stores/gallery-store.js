557f98079604afeb18fe9617ec38decbfbf61e67
shahaf noked <shahafnoked@gmail.com>
2024-09-23 14:55:22 +0300

    moved imgs to public folder

--- stores/gallery-store.js
+++ stores/gallery-store.js
@@ -3,25 +3,25 @@ import { create } from 'zustand';
 const useGalleryStore = create((set) => ({ 
   images: [
     {
-      thumbnail: '/src/assets/images/image-product-1-thumbnail.jpg',
-      full: '/src/assets/images/image-product-1.jpg',
+      thumbnail: '/images/image-product-1-thumbnail.jpg',
+      full: '/images/image-product-1.jpg',
     },
     {
-      thumbnail: '/src/assets/images/image-product-2-thumbnail.jpg',
-      full: '/src/assets/images/image-product-2.jpg',
+      thumbnail: '/images/image-product-2-thumbnail.jpg',
+      full: '/images/image-product-2.jpg',
     },
     {
-      thumbnail: '/src/assets/images/image-product-3-thumbnail.jpg',
-      full: '/src/assets/images/image-product-3.jpg',
+      thumbnail: '/images/image-product-3-thumbnail.jpg',
+      full: '/images/image-product-3.jpg',
     },
     {
-      thumbnail: '/src/assets/images/image-product-4-thumbnail.jpg',
-      full: '/src/assets/images/image-product-4.jpg',
+      thumbnail: '/images/image-product-4-thumbnail.jpg',
+      full: '/images/image-product-4.jpg',
     },
   ],
   // State for active image index and URL
   activeIndex: 0, // Default index
-  activeImgUrl: '/src/assets/images/image-product-1.jpg', // Default image
+  activeImgUrl: '/images/image-product-1.jpg', // Default image
   
   // Function to update active index and image URL
   setActiveIndex: (newIndex) => set((state) => {
